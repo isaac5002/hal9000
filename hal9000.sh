d152c86d62e4f90765b9ae983762f2294cb0a904
Isaac <156618303+isaac5002@users.noreply.github.com>
2025-12-03 07:02:56 +0000

    Adds HAL program

---
+++ hal9000.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#A script to demo user input and variable use
+
+echo "Hello. I'm HAL 9000. WHat is your name?"
+
+read username
+
+echo "I'm sorry $username. I'm afraid I cant open the pod bay doors. "
