23a14b219027e74c0187cdfc48fe869cfd031ae4
Isaac <156618303+isaac5002@users.noreply.github.com>
2025-12-03 07:06:33 +0000

    Adds readme

--- hal9000.sh
+++ hal9000.sh
@@ -6,4 +6,4 @@ echo "Hello. I'm HAL 9000. WHat is your name?"
 
 read username
 
-echo "I'm sorry $username. I'm afraid I cant open the pod bay doors. "
+echo "I'm sorry $username. I'm afraid I can not open the pod bay doors. "
